399692d4b772f72c1cf5b10f301b7664fe6d7da0
Jorge Silva <jorge@syndicate.io>
2025-08-06 19:11:07 +0100

    cool remaps / opts

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,17 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- modes:
+-- n -- Normal mode -- Regular mode when you’re navigating around
+-- i -- Insert mode -- When you’re typing text
+-- v -- Visual mode (charwise) -- When you visually select text (charwise)
+-- x -- Visual mode (exclusive) -- Similar to v but behaves slightly differently internally
+-- s -- Select mode -- Like visual, but behaves like insert
+-- o -- Operator-pending mode -- While waiting for a movement after an operator (e.g., d, y)
+-- ! -- Insert or command-line mode -- For mapping in insert or cmd-line mode
+-- t -- Terminal mode -- In terminal buffers
+-- c -- Command-line mode -- When typing commands after :
+
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
@@ -11,3 +22,37 @@ map("i", "jk", "<ESC>")
 -- keep cursor in the middle of the screen when paging up/down
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
+
+-- keeps the cursor in the middle of the screen when searching
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+--keep the contents of the _ register when pasting over a selection
+map("x", "<leader-p>", "_dP")
+
+-- map a decent keys to telescope stuff
+map("n", "<C-p>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true, desc = "telescope find files" })
+map("n", "<C-f>", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true, desc = "telescope live grep" })
+
+-- allow to move selected lines up/down
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+--use leader to copy to system clipboard
+map("n", "<leader>y", '"+y', { desc = "copy to system clipboard" })
+map("v", "<leader>y", '"+y', { desc = "copy to system clipboard" })
+map("n", "<leader>Y", '"+Y', { desc = "copy to system clipboard" })
+
+-- navigate "quick-fix list" (commenting as I have no clue wtf that is lol)
+-- map("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- map("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- map("n", "<leader>k", "<cmd>cnext<CR>zz")
+-- map("n", "<leader>j", "<cmd>cprev<CR>zz")
+
+-- replace occurances of the current word
+vim.keymap.set(
+  "n",
+  "<leader>s",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "replace all occurances current word" }
+)
--- lua/options.lua
+++ lua/options.lua
@@ -6,3 +6,13 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 vim.opt.relativenumber = true
+
+-- don't do backups, but let me keep undo's for days
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 8 -- keep 8 lines, don't let the cursor hit the bottom of the page
+
+vim.opt.clipboard = "" -- disables use of system clipboard
