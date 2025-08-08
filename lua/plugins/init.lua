d1cebbc70fd9ba3cd6e1ebb2501ea8a440bfa2c2
Jorge Silva <jorge@syndicate.io>
2025-08-08 18:04:51 +0100

    snipe

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -65,6 +65,20 @@ return {
     },
   },
 
+  {
+    "leath-dub/snipe.nvim",
+    keys = {
+      {
+        "gb",
+        function()
+          require("snipe").open_buffer_menu()
+        end,
+        desc = "Open Snipe buffer menu",
+      },
+    },
+    opts = {},
+  },
+
   ------------------------------------------------------------------
   --- RUST STUFF
   ------------------------------------------------------------------
