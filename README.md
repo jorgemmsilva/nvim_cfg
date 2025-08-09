9fc699f70b9e3cf8f916fc65589da6ebc4f4294d
Jorge Silva <jorge@syndicate.io>
2025-08-09 23:02:03 +0100

    harpoon

---
+++ README.md
@@ -0,0 +1,6 @@
+# My nvim configuration
+
+
+
+## Recommended to install:
+https://github.com/dandavison/delta
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -197,6 +197,68 @@ return {
     },
   },
 
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    init = function()
+      local harpoon = require "harpoon"
+      harpoon:setup()
+      local conf = require("telescope.config").values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        local make_finder = function()
+          local paths = {}
+
+          for _, item in ipairs(harpoon_files.items) do
+            table.insert(paths, item.value)
+          end
+
+          return require("telescope.finders").new_table {
+            results = paths,
+          }
+        end
+
+        require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table {
+              results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+            attach_mappings = function(prompt_buffer_number, map)
+              -- delete entries from the telescope list
+              map("i", "<C-d>", function()
+                local state = require "telescope.actions.state"
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_buffer_number)
+
+                harpoon:list():remove(selected_entry)
+                current_picker:refresh(make_finder())
+              end)
+
+              return true
+            end,
+          })
+          :find()
+      end
+
+      -- add to the list
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():add()
+      end)
+      -- toggle harpoon menu
+      vim.keymap.set("n", "<C-e>", function()
+        toggle_telescope(harpoon:list())
+      end, { noremap = true, desc = "Open harpoon window" })
+    end,
+  },
+
   ------------------------------------------------------------------
   --- RUST
   ------------------------------------------------------------------
