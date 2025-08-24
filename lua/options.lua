84e2840172ee7fbeb08346eaeeb7cd2407692c5e
Jorge Silva <jorge@syndicate.io>
2025-08-24 18:02:15 +0100

    change terminal window title

--- lua/options.lua
+++ lua/options.lua
@@ -64,3 +64,7 @@ vim.g.loaded_ruby_provider = 0
 -- local sep = is_windows and "\\" or "/"
 -- local delim = is_windows and ";" or ":"
 -- vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
+
+vim.opt.title = true
+vim.opt.titlelen = 0 -- do not shorten
+vim.opt.titlestring = 'nvim %{expand("%:p")}'
